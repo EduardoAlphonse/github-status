21a8c846739f72b2b7b8c9fb44d45c8300bdc0ea
EduardoAlphonse <eduardo.afoli@gmail.com>
2021-07-13 00:32:23 -0300

    Updates

---
+++ precache-manifest.b4a3d5406c4df7119f85c6d3450a3610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d75193a7c972aa46ae257dc080d29fa",
+    "url": "/github-status/index.html"
+  },
+  {
+    "revision": "8ab833b053e7396b439a",
+    "url": "/github-status/static/css/main.8e22b32e.chunk.css"
+  },
+  {
+    "revision": "6ad9d9fbb07e24067f00",
+    "url": "/github-status/static/js/2.04e91b6d.chunk.js"
+  },
+  {
+    "revision": "91dbce2febfc227edf628f0fd0b15354",
+    "url": "/github-status/static/js/2.04e91b6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab833b053e7396b439a",
+    "url": "/github-status/static/js/main.9c694953.chunk.js"
+  },
+  {
+    "revision": "bcb325c6e942b7aff512",
+    "url": "/github-status/static/js/runtime-main.492ad581.js"
+  }
+]);
